549453c15f2c63c55c5a697f22568d9b3fd808b5
valdofal <v.dofal@gmail.com>
2020-10-26 16:28:04 +0100

    iniToJSON améliorée

--- main.js
+++ main.js
@@ -28,12 +28,20 @@ function iniToJSON (){
     regexp2 = RegExp(/^[[\w][^\n]*/gm)
     fs.readFile(fileName,'utf8', function (err,data) {
         let comments = data.match(regexp2)
-        console.log(comments)
         let result =''
+        let compt = 1
         for(let i = 0; i < comments.length; i ++){
-            result += '"'+comments[i].replace('=','":"')+'"'            
+           if(comments[i].startsWith('[') && comments[i].endsWith(']')){
+               result += comments[i].toString().match(/\w+/gm) + '\n{\n'
+               console.log(comments[i].match(/\w+/gm))
+           }
+           else{
+                result += '"'+comments[i].replace('=','":"')+'"'     
+                if(comments[i+1].startsWith('[')){
+                    result +='\n }\n'
+                }      
+           }
         }
-        result += '\n}'
         result = result.replace(/""/g,'",\n"')
 
 
